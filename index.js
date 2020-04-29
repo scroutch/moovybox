92db38d729aa34111c22bf74472f4d68852fe51d
Nicolas Garçon <nicolas.garcon@gmail.com>
2020-04-29 20:05:56 +0200

    Router + home and 404 routes

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ require('dotenv').config();
 const PORT = process.env.PORT || 5050; 
 const app = express(); 
 
+app.use(require('./app/router')); 
 
 app.listen(PORT, _ => console.log("Server running on ", PORT)); 
