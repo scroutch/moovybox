d25a594b00b1cc74883aaf65d61d6da2427dbd74
Nicolas Garçon <nicolas.garcon@gmail.com>
2020-04-29 19:59:00 +0200

    Server running

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("salut");
+const express = require('express');
+require('dotenv').config(); 
+const PORT = process.env.PORT || 5050; 
+const app = express(); 
+
+
+app.listen(PORT, _ => console.log("Server running on ", PORT)); 
