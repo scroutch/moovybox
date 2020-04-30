be298f929804511cf90fa41595d80c366f487810
Nicolas Garçon <nicolas.garcon@gmail.com>
2020-04-30 14:15:35 +0200

    user signup OK

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const PORT = process.env.PORT || 5050; 
 const app = express(); 
 
+app.use(express.urlencoded({extended: true}));
+
 app.use(require('./app/router')); 
 
 app.listen(PORT, _ => console.log("Server running on ", PORT)); 
