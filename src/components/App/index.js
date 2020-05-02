519e5e6be95d8143d518444829b70667aba89ee0
scroutch <cecile.duhain@gmail.com>
2020-05-02 18:04:25 +0200

    Carousel Homepage done

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,8 @@
 // == Import npm
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import Home from '../Home';
+
 
 // == Import
 // import reactLogo from './react-logo.svg';
@@ -9,18 +11,10 @@ import './styles.css';
 // == Composant
 const App = () => {
   const dispatch = useDispatch();
-  const clickCount = useSelector((state) => state.counter);
 
   return (
     <div className="app">
-      {/* <img src={reactLogo} alt="react logo" /> */}
-      <h1>Composant : App</h1>
-      <button
-        type="button"
-        onClick={(evt) => dispatch({ type: 'INCREMENT' })}
-      >
-        Clic-me ! ({clickCount})
-      </button>
+      <Home />
     </div>
   );
 };
