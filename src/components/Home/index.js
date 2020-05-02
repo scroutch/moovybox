6d34e46e1ae089e16969b27af68243de7401bbf1
scroutch <cecile.duhain@gmail.com>
2020-05-02 19:55:08 +0200

    added description and button in homepage

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,65 @@
 import React from 'react';
 import Slideshow from '../Slide';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import './styles.css';
+
+const useStyles = makeStyles({
+    card: {
+        width: "70%",
+        margin: "auto",
+        marginBottom: 20,
+    },
+    root: {
+      minWidth: 275,
+    },
+    title: {
+      fontSize: 25,
+    },
+    button: {
+        width: "90%",
+        margin: "auto"
+    },
+    button2: {
+        width: "50%",
+        margin: "auto"
+    },
+  });
 
 const Home = () => {
+    const classes = useStyles();
     return(
         <div>
+            <Card className={classes.card} variant="outlined">
+                <CardContent>
+                    <Typography className={classes.title} color="textSecondary" gutterBottom>
+                        MoovyBox
+                    </Typography>
+                    <Typography variant="h5" component="h2">
+                        Pour mieux gérer vos déménagements
+                    </Typography>
+                    <Typography variant="body2" component="p">
+                        Sint sit ea occaecat culpa est in consequat magna et duis dolor tempor tempor. Officia velit labore adipisicing officia non eu enim quis adipisicing labore. Eu labore ipsum sit laboris magna est commodo nostrud consectetur aliqua elit. Esse irure nostrud cupidatat fugiat enim pariatur.
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button className={classes.button} size="small" variant="contained" color="primary" href="#">Je suis déjà inscris, je me connecte</Button>
+                </CardActions>
+                <CardActions>
+                    <Button className={classes.button2} size="small" variant="contained" color="primary" href="#">Je m'inscris</Button>
+                </CardActions>
+            </Card>
             <Slideshow />
+            <div className="contain-btn">
+                <Button className="btn2" size="small" variant="contained" color="primary" href="#" endIcon={<ChevronRightIcon />}>Ca cartonne</Button>
+            </div>
+
+
         </div>
     )
 }
