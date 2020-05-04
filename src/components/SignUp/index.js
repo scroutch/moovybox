d97271a322444844ec4787d8f8a39f52906d8f69
BarbierCarole <carole83460@gmail.com>
2020-05-04 14:08:00 +0200

    signup validation ok

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -5,11 +5,9 @@ import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
@@ -18,11 +16,21 @@ import Button from '../modules/components/Button';
 import Footer from '../modules/views/Footer';
 import Header from '../modules/views/Header';
 import { FormHelperText } from '@material-ui/core';
-import { Formik } from "formik";
-import * as Yup from "yup";
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 
 const Schema = Yup.object().shape({
-  password: Yup.string().required('Ce champ est requis'),
+  nickname: Yup.string().required('Requis'),
+  email: Yup.string().required('Requis')
+    .matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, 'Email invalide'),
+  password: Yup.string()
+    .min(8, 'Le mot de passe doit avoir minimum 8 caractères')
+    .max(20, 'Mot de passe > 20 caractères')
+    .matches(
+      /^.*(?=.{8,})((?=.*[#?!@$%^&*-]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
+      "Doit contenir au moins 8 caractères dont une majuscule, une minuscule, un chiffre et un caractère spécial suivant #?!@$%^&*-"
+    )
+    .required('Requis'),
   changepassword: Yup.string().when('password', {
     is: (val) => (!!(val && val.length > 0)),
     then: Yup.string().oneOf(
@@ -63,13 +71,20 @@ const SignUp = () => {
   return (
     <Formik
       initialValues={{
-        password: "",
-        changepassword: ""
+        nickname: '',
+        email: '',
+        password: '',
+        changepassword: '',
       }}
       validationSchema={Schema}
-      onSubmit={() => {}}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400)}
+      }
     >
-      {({ values, errors, handleSubmit, handleChange, handleBlur }) => {
+      {({ values, errors, touched, handleSubmit, handleChange, handleBlur }) => {
         return (
           <div className={classes.root}>
             <CssBaseline />
@@ -83,11 +98,13 @@ const SignUp = () => {
                 <Typography component="h1" variant="h4">
                   Création de compte
                 </Typography>
-                <form className={classes.form} 
+                <form
+                  className={classes.form} 
                   // onSubmit={(evt) => {
                   //   evt.preventDefault();
                   //   // dispatch(sendMessage());
                   // }}
+                  noValidate
                   onSubmit={handleSubmit}
                 >
 
@@ -95,15 +112,23 @@ const SignUp = () => {
                     <Grid item xs={12}>
                       <TextField
                         autoComplete="nickname"
-                        name="nickename"
+                        name="nickname"
                         variant="outlined"
                         required
                         fullWidth
                         id="nickname"
                         label="Pseudo"
                         autoFocus
+                        helperText="Pseudo requis"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.nickname}
                       />
-                      <FormHelperText id="my-helper-text-nickname">Email requis</FormHelperText>
+                      {errors.nickname && touched.nickname ? (
+                        <span className="error" style={{ color: "red" }}>
+                          {errors.nickname}
+                        </span>
+                      ) : null}
                     </Grid>
                     <Grid item xs={12}>
                       <TextField
@@ -115,8 +140,16 @@ const SignUp = () => {
                         name="email"
                         autoComplete="email"
                         type="email"
+                        helperText="Email requis"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.email}
                       />
-                      <FormHelperText id="my-helper-text-email">Email requis</FormHelperText>
+                      {errors.email && touched.email ? (
+                        <span className="error" style={{ color: "red" }}>
+                          {errors.email}
+                        </span>
+                      ) : null}
                     </Grid>
                     <Grid item xs={12}>
                       <TextField
@@ -128,45 +161,35 @@ const SignUp = () => {
                         type="password"
                         id="password"
                         autoComplete="current-password"
+                        helperText="Champs Requis - Minimum 1 minuscule, 1 majuscule, 1 chiffre, un des caractères #?!@$%^&*-"
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        value={values.password}
                       />
-                      <FormHelperText id="my-helper-text-psw"> Champs Requis - Minimum 1 minuscule, 1 majuscule, 1 chiffre, un des caractères #?!@$%^&*-</FormHelperText>
+                      <span className="error" style={{ color: "red" }}>
+                        {errors.password}
+                      </span>
                     </Grid>
                     <Grid item xs={12}>
                       <TextField
                         variant="outlined"
                         required
                         fullWidth
-                        name="passwordControl"
+                        name="changepassword"
                         label="Mot de passe"
                         type="password"
-                        id="passwordControl"
+                        id="changepassword"
                         autoComplete="current-password"
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        value={values.changepassword}
                       />
                       <FormHelperText id="my-helper-text-psw2">Veuillez saisir à nouveau le mot de passe requis</FormHelperText>
+                      <span className="error" style={{ color: "red" }}>
+                        {errors.changepassword}
+                      </span>
                     </Grid>
-                    <label for="passowrd">Password</label>
-                    <input
-                      type="password"
-                      name="password"
-                      onBlur={handleBlur}
-                      onChange={handleChange}
-                      value={values.password}
-                    />
-                    <span className="error" style={{ color: "red" }}>
-                      {errors.password}
-                    </span>
-
-                    <label for="passowrd">Confirm Password</label>
-                    <input
-                      type="password"
-                      name="changepassword"
-                      onBlur={handleBlur}
-                      onChange={handleChange}
-                      value={values.changepassword}
-                    />
-                    <span className="error" style={{ color: "red" }}>
-                      {errors.changepassword}
-                    </span>
+                    
                   </Grid>
                   <Button
                     type="submit"
