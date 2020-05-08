1168e1d2600b1dd66bc5cee3f3341c36bf2f2368
BarbierCarole <carole83460@gmail.com>
2020-05-08 16:16:57 +0200

    signup -server ok validation to do and stay connected to see

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,33 +1,33 @@
 import {
-  SYNC_NICKNAME,
+  SYNC_PSEUDO,
   SYNC_EMAIL,
   SYNC_PASSWORD,
+  SYNC_PASSWORDVAL,
 } from './actions';
 
 const initialState = {
   counter: 0,
-  nickname: '',
-  users: [
-    {
-      nickname: 'nirrep',
-      email: 'nirrep@gmail.com',
-    },
-  ],
+  pseudo: '',
   email: '',
   password: '',
+  passwordVal: '',
+
 };
 
 
 export default (state = initialState, action = {}) => {
   switch (action.type) {
-    case SYNC_NICKNAME: {
-      return { ...state, nickname: action.nickname };
+    case SYNC_PSEUDO: {
+      return { ...state, pseudo: action.pseudo };
     }
     case SYNC_EMAIL: {
       return { ...state, email: action.email };
     }
     case SYNC_PASSWORD: {
       return { ...state, password: action.password };
+    }
+    case SYNC_PASSWORDVAL: {
+      return { ...state, passwordVal: action.passwordVal };
     }
     case 'INCREMENT': {
       return {
