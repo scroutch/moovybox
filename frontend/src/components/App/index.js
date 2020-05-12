6674b09a62881902fddd05e2843d1cfe9ef7d9a5
Nicolas Garçon <nicolas.garcon@gmail.com>
2020-05-12 09:28:49 +0200

    front wrap into frontend folder

---
+++ frontend/src/components/App/index.js
@@ -0,0 +1,81 @@
+// == Import npm
+import React from 'react';
+import Home from '../Home';
+
+import { useSelector } from 'react-redux';
+import { Route, Redirect, Switch } from 'react-router';
+
+// == Import
+import './styles.css';
+import SignIn from 'src/components/SignIn';
+import Move from 'src/components/Move';
+import FormMove from 'src/components/FormMove';
+import SignUp from 'src/components/SignUp';
+import ResetPassword from 'src/components/ResetPassword';
+import Profile from 'src/components/Profile';
+import Contact from '../Contact';
+import CreateBox from 'src/components/CreateBox';
+
+
+
+// == Composant
+const App = () => {
+
+  const email = useSelector((state) => state.email);
+  const password = useSelector((state) => state.password);
+
+  return (
+    <div className="app">
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route
+
+          exact
+          path="/move"
+          render={() => {
+            if ((email === '') || (password === '')) {
+              console.log('email,password page App/index',email,password);
+              return <Redirect to="/signin" />;
+              
+            }
+            console.log('email,password,',email,password);
+            return <Move />;
+          }}
+        />
+        
+        <Route exact path="/signin">
+          <SignIn />
+        </Route>
+
+        <Route exact path="/signup">
+          <SignUp />
+        </Route>
+        <Route exact path="/profile">
+          <Profile />
+        </Route>
+        <Route exact path="/ResetPassword">
+          <ResetPassword />
+        </Route>
+        <Route exact path="/move">
+          <Move />
+        </Route>
+        <Route exact path="/create-move">
+          <FormMove />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+        <Route exact path="/create-box">
+          <CreateBox />
+        </Route>
+        <Route>404</Route>
+      </Switch>
+    </div>
+  );
+};
+
+// == Export
+export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable react/jsx-indent */
+// == Import : npm
+import React from 'react'; // couche 1
+import { render } from 'react-dom'; // couche 2
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import axios from 'axios';
+
+// == Import : local
+// Composants
+import App from 'src/components/App';
+import store from 'src/store';
+
+// axios
+//   .get('http://', {
+//     withCredentials: true,
+//   })
+//   .then((response) => console.log('reponse axios', response));
+
+
+// == Render
+// 1. Élément React racine (celui qui contient l'ensemble de l'app)
+//    => crée une structure d'objets imbriqués (DOM virtuel)
+const rootReactElement = (
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>
+
+);
+// 2. La cible du DOM (là où la structure doit prendre vie dans le DOM)
+const target = document.getElementById('root');
+// 3. Déclenchement du rendu de React (virtuel) => DOM (page web)
+render(rootReactElement, target);
