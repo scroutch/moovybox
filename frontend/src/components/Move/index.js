410da5f2ff99e3dfbbeb5008be0104b81812c33b
scroutch <cecile.duhain@gmail.com>
2020-05-14 18:03:26 +0200

    Add css to form List

--- frontend/src/components/Move/index.js
+++ frontend/src/components/Move/index.js
@@ -3,36 +3,37 @@ import withRoot from '../modules/withRoot';
 import Footer from '../modules/views/Footer';
 import Header from '../modules/views/Header';
 import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 import AddIcon from '@material-ui/icons/Add';
 import Fab from '@material-ui/core/Fab';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import ListItemText from '@material-ui/core/ListItemText';
+import Button from '@material-ui/core/Button';
 import DeleteIcon from '@material-ui/icons/Delete';
-import IconButton from '@material-ui/core/IconButton';
 import Tooltip from '@material-ui/core/Tooltip';
+import moment from 'moment';
 import {BrowserRouter as Router, Link} from "react-router-dom";
 import axios from 'axios';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
+    alignContent: 'center',
     flexDirection: 'column',
     minHeight: '100vh',
   },
-  fab: {
-    margin: theme.spacing(2),
-  },
-  absolute: {
-    position: 'absolute',
-    bottom: theme.spacing(2),
-    right: theme.spacing(3),
-  },
+
   liste: {
-    textAlign: 'center'
+    marginTop: theme.spacing(5),
+    textAlign: 'center',
+
+  },
+  btn: {
+    width: '60%',
+    padding: theme.spacing(2),
+    marginBottom: theme.spacing(2)
+  },
+  titre: {
+    textAlign: 'center',
+    paddingTop: theme.spacing(5)
   }
 }));
 
@@ -49,17 +50,31 @@ const Move = () => {
            console.log(err);
          })
   }, []);
+
+  const handleDelete = (moveToDelete) => () => {
+    setMoves((moves) => moves.filter((move) => move.key !== moveToDelete.key));
+  };
+
   return (
     <div className={classes.root}>
       <Header />
+      <Link to="/create-move">
+      <Typography component="h1" variant="h4" className={classes.titre}>
       <Tooltip title="Add" aria-label="add">
         <Fab color="primary" className={classes.fab}>
-          <Link to="/create-move"><AddIcon /></Link>
+          <AddIcon />
+
         </Fab>
       </Tooltip>
+      Ajouter un déménagement
+      </Typography>
+      </Link>
         <ul className={classes.liste}>
           {moves.map(move => <li key={move.id}>
-            {move.label}{move.address}{move.date}
+            <Button variant="outlined" color="primary" href="/create-box" className={classes.btn} >
+               {move.label} {move.address} {moment(move.date).format('MM-DD-YYYY')}
+            </Button>
+            <DeleteIcon fontSize="large" color="secondary" />
             </li>)}
         </ul>
       <Footer />
